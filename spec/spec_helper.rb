85d9e9baea827c40d7fcd141f826304dadb08350
Alberto Bajo <albertobajo@gmail.com>
2013-11-11 14:25:04 +0100

    factory_girl_rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require "factory_girl_rails"
+require "friendly_id"
+require 'shoulda/matchers'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
